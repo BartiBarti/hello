be96b289851e61a6bb00ec08c0b9d45fcd261509
BartiBarti <bakos@o2.pl>
2018-07-12 10:34:39 +0200

    pierwszy commit

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+
+
+    {
+        System.out.println("Bart");
+        System.out.println("email");
+        System.out.println("Bla bla bla car");
+        System.out.println("Nothing");
+        System.out.println("bla");
+
+    }
+
 }
